7e63f3ce040aadf4f1eb9a3dbc8ffc20508edad3
hanxiaosheng <2695621767@qq.com>
2021-08-13 11:28:55 +0800

    冒泡排序

---
+++ src/BubbleSort.java
@@ -0,0 +1,37 @@
+
+/*冒泡算法*/
+
+public class BubbleSort {
+
+    public  static  void bubbleSort(int[] arr){
+        for (int i = 0; i <arr.length ; i++) {  // 外部循环
+            boolean isSorted = true;
+            for (int j = 0; j < arr.length-i-1; j++) {
+
+                if (arr[j]>arr[j+1]){
+                    int temp = arr[j];
+                    arr[j] = arr[j+1];
+                    arr[j+1] = temp;
+                    isSorted = false;
+                }
+            }
+            if (isSorted){
+                break;
+            }
+        }
+    }
+
+    public static void main(String[] args) {
+
+//        removeKdigits("1593212",3);
+        int[] arr = new int[] { 2, 8, 7, 9, 4, 1, 5, 0 };
+         bubbleSort(arr);
+        for (int i = 0; i < arr.length; i++) {
+            System.out.print(arr[i]);
+        }
+    }
+
+
+
+
+}
