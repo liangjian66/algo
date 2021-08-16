77ee0f09622e039eca0d6eff6d5dec058eb270c8
hanxiaosheng <2695621767@qq.com>
2021-08-16 10:57:51 +0800

    计数排序
    计数排序1

--- src/BubbleSort.java
+++ src/BubbleSort.java
@@ -21,7 +21,15 @@ public class BubbleSort {
         }
     }
 
+    public static void main(String[] args) {
 
+//        removeKdigits("1593212",3);
+        int[] arr = new int[] { 2, 8, 7, 9, 4, 1, 5, 0 };
+        bubbleSort(arr);
+        for (int i = 0; i < arr.length; i++) {
+            System.out.print(arr[i]);
+        }
+    }
 
 
 
---
+++ src/CountSort.java
@@ -0,0 +1,46 @@
+/*计数排序*/
+
+
+public class CountSort {
+
+
+    public static  int[] countSort(int[] array){
+        int max = array[0];
+        for (int i = 1; i < array.length; i++) {
+            if (array[i]>max){
+                max = array[i];
+            }
+
+        }
+        // 根据数列的最大值确定统计数组的长度
+        int[] countArray = new int[max+1];
+        // 遍历数列 ， 填充统计数组f
+        for (int i = 0; i < array.length; i++) {
+            countArray[array[i]]++;
+        }
+
+        // 遍历统计数组 ， 输出结果
+        int index = 0;
+        int[] sortedArray = new int[array.length];
+        for (int i = 0; i < countArray.length; i++)
+        {
+            // countArray 是几就添加几次
+            for (int j = 0; j < countArray[i]; j++)
+            {
+                sortedArray[index++] = i;
+            }
+        }
+        return  sortedArray;
+
+    }
+
+    public static void main(String[] args) {
+
+//        removeKdigits("1593212",3);
+        int[] arr = new int[] { 2, 8, 7, 9, 4, 1, 5, 0 };
+        int[] newArr =  countSort(arr);
+        for (int i = 0; i < arr.length; i++) {
+            System.out.print(newArr[i]);
+        }
+    }
+}
