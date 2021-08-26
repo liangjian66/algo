c1ab3bc7926f1f647c54aee6c6b895ebbb2bedf3
hanxiaosheng <2695621767@qq.com>
2021-08-26 16:35:35 +0800

    归并排序 整理
    归并排序 整理

--- src/mergeSort.java
+++ src/mergeSort.java
@@ -5,13 +5,10 @@ public class mergeSort {
          int mid = rightPtr-1;
          int i = leftPtr;
          int j = rightPtr;
-
-
          int k = 0;
          int[] temp = new int[rightBound-leftPtr+1];
          while (i<=mid && j<=rightBound){
              temp[k++] = arr[i]<=arr[j]?arr[i++]:arr[j++];
-
          }
          // 1. 假设i<=mid 就是上半段有剩余
         while (i<=mid){
@@ -20,19 +17,12 @@ public class mergeSort {
         while (j<=rightBound){
             temp[k++] = arr[j++];
         }
-//        System.out.print(arr);
-//        arr = temp;
         for (int m = 0; m < temp.length; m++) {
             arr[leftPtr+m] = temp[m];
-//            System.out.print(temp[m]);
-
         }
-
-//        return  temp;
     }
-/*递归相关内容*/
+/*递归相关内容： 终止条件 单层内容*/
     public  static  void sort(int[] arr,int left ,int right){
-
         // 终止条件
         if (left == right) return ;
 //           return   mergeSort(arr);
@@ -45,16 +35,13 @@ public class mergeSort {
 
 //    先确保两个 有序数组 才能merge
         mergeSort(arr,left,mid+1,right);
-
     }
 
 
     public static void main(String[] args) {
-
 //        removeKdigits("1593212",3);
-
         int[] arr = new int[] { 1, 4, 7, 8, 3, 6, 9 };
-        sort(arr,0,4);
+        sort(arr,0,6);
 //        int[] tempArr =    sort(arr);
         for (int i = 0; i < arr.length; i++) {
             System.out.print(arr[i]);
