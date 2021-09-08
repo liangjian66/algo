135d11ab69bacfc3aa18fe1fffe36a073b920e61
hanxiaosheng <2695621767@qq.com>
2021-09-08 11:42:51 +0800

    选择排序
    选择排序

--- src/MiddleSolution.java
+++ src/MiddleSolution.java
@@ -1,4 +1,6 @@
 import java.util.ArrayList;
+import java.util.Deque;
+import java.util.LinkedList;
 import java.util.List;
 
 /*二叉树 中序遍历*/
@@ -17,4 +19,23 @@ public class MiddleSolution {
         res.add(root.val);
         preorder(root.right, res);
     }
+
+
+    public List<Integer> inorderTraversal(TreeNodeNew root) {
+        List<Integer> res = new ArrayList<Integer>();
+        Deque<TreeNodeNew> stk = new LinkedList<TreeNodeNew>();
+
+
+        while (root != null || !stk.isEmpty()) {
+            while (root != null) {
+                stk.push(root);
+                root = root.left;
+            }
+            root = stk.pop();
+            res.add(root.val);
+            root = root.right;
+        }
+        return res;
+    }
+
 }
