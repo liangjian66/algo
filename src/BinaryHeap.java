1036b34dcdbe9dc07284cd45a7a7b638c6af975a
hanxiaosheng <2695621767@qq.com>
2021-09-09 16:25:30 +0800

    二叉堆 下沉调整
    二叉堆 下沉调整

--- src/BinaryHeap.java
+++ src/BinaryHeap.java
@@ -20,13 +20,32 @@ public class BinaryHeap {
         array[childIndex] = temp;
 
     }
-
+    // 最小堆  做下沉
     public  static  void downAdjust(int[]  array ,int parentIndex , int length){
         //下沉
         // temp 保存父节点的值 ，用于最后的赋值
         int temp = array[parentIndex];
         int childIndex = 2*parentIndex+1;
         while (childIndex < length){
+//             如果有右孩子 ，且右孩子 小于 左孩子的值 则定位到右孩子
+            if (childIndex+1<length && array[childIndex+1] < array[childIndex]){
+                childIndex++;
+            }
+            // 如果父节点小于任何一个孩子的值 ， 则直接退出
+            if (temp < array[childIndex]) break;
+            // 无须真正交换 ， 单向赋值即可
+            array[parentIndex] = array[childIndex];
+            parentIndex = childIndex;
+            childIndex = 2* childIndex + 1;
+
+        }
+        array[parentIndex]  = temp;
+    }
+
+    public static  void  buildHeap(int[] array){
+        // 从最后一个非叶子节点开始 ， 依次做下沉操作
+        for (int i = (array.length-2)/2; i >=0 ; i--) {
+            downAdjust(array , i ,array.length);
 
         }
     }
@@ -35,6 +54,11 @@ public class BinaryHeap {
         int[] array = new int[] {1,3,2,6,5,7,8,9,10,0};
         upAdjust(array);
         System.out.print(Arrays.toString(array));
+
+        int[] arrayTwo = new int[] {7,1,3,10,5,2,8,9,6};
+        buildHeap(arrayTwo);
+        System.out.print(Arrays.toString(arrayTwo));
+
     }
 
 
