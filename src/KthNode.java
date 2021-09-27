fbd1b31ac931f633f656d03fcf4c03409c767341
hanxiaosheng <2695621767@qq.com>
2021-09-27 11:29:36 +0800

    二叉搜索树的第K大节点
    二叉搜索树的第K大节点

---
+++ src/KthNode.java
@@ -0,0 +1,35 @@
+import java.util.ArrayList;
+
+public class KthNode {
+
+//    ArrayList<TreeNode> res;
+
+//    {
+//        res = new ArrayList<TreeNode>();
+//    }
+
+    //    中序遍历 的倒序列
+    public  void middleSort(TreeNode rootNode ,ArrayList res ){
+        if (rootNode == null){
+            return;
+        }
+        middleSort(rootNode.right,res);
+        res.add(rootNode);
+        middleSort(rootNode.left,res);
+    }
+
+    public  int kthNode(TreeNode root,int k){
+        ArrayList<TreeNode> res = new ArrayList<TreeNode>();
+        middleSort(root,res);
+        for (int i = 0; i < res.size(); i++) {
+            if (i == k-1){
+                TreeNode kNode = res.get(i);
+                return kNode.data;
+            }
+        }
+
+        return 0;
+
+    }
+
+}
