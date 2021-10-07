913b7743dc2f8d1e8d314c9e62d0362ff2081ad3
hanxiaosheng <2695621767@qq.com>
2021-10-07 14:12:11 +0800

    第一个错误的版本
    第一个错误的版本

---
+++ src/FirstBadVersion.java
@@ -0,0 +1,20 @@
+public class FirstBadVersion {
+
+    public  int firstBadVersion(int n){
+         int left =  1;
+         int right = n;
+//         迭代  循环直至区间左右端点 相同
+         while (left<right){
+             int mid = left + (right - left)/2;
+             if (isBadVersion(mid)){
+                 right = mid; // 答案在区间 [left, mid] 中
+
+             }else {
+                 left = mid + 1; // 答案在区间 [mid+1, right] 中
+
+             }
+         }
+        // 此时有 left == right，区间缩为一个点，即为答案   激进的国际化
+        return left;
+    }
+}
