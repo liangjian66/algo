5f71db9e0d005b0a136e04e40988e79f96061740
hanxiaosheng <2695621767@qq.com>
2021-10-07 11:00:52 +0800

    1
    1

--- src/IsSymmetrical.java
+++ src/IsSymmetrical.java
@@ -5,20 +5,21 @@ public class IsSymmetrical {
 
     public boolean isSymmetric(TreeNode root){
         if (root==null)  return true;
-        return  recur(root.left,root.right);
+        return  judge(root,root);
+    }
+
+    public  boolean judge(TreeNode left,TreeNode right){
+        if (left == null && right==null){
+            return  true;
+        }
+        if (left == null || right == null ||left.val != right.val){
+            return  false;
+        }
+        return  ((left.val==right.val)&&(judge(left.left,right.right))&&(judge(left.right,right.left)));
     }
 
 
-      boolean recur(TreeNode L,TreeNode R){
-          if (L==null&&R==null) return  true;
-          if (L == null ||R==null|| L.val != R.val){
-              return  false;
-          }
-          Boolean leftBool = recur(L.left, R.right);
-          Boolean rightBool = recur(L.right, R.left);
 
-          return  leftBool&&rightBool;
-      }
 
 
 
