2410f5dcb59527fcb4c75f479a94753ab964d325
hanxiaosheng <2695621767@qq.com>
2021-10-07 11:31:05 +0800

    大于给定元素的最小元素
    大于给定元素的最小元素

---
+++ src/NextGreatestLetter.java
@@ -0,0 +1,28 @@
+public class NextGreatestLetter {
+
+    public  char nextGreatestLetter(char[] letters, char target){
+        if (letters == null){
+            return  ' ';
+        }
+//        二分查找
+//        双指针
+        int i = 0 ;
+        int j= letters.length-1;
+        char ans = ' ';
+        while (i<j){
+            if (letters[j]-target>0){
+                return  letters[i];
+            }
+            int mid  = i+ (j-i)/2;
+            if (letters[mid]-target<=0){   //中间值较小
+//                找到最后一个比target 小的字母
+                 i = mid+1;
+            }else {
+                j= mid-1;
+            }
+
+        }
+        return letters[i];
+
+    }
+}
