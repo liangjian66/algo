21ad66477e202e66f39a02412ec3f74818ea6a6e
hanxiaosheng <2695621767@qq.com>
2021-10-15 13:37:48 +0800

    在单链表中删除指定值的节点
    在单链表中删除指定值的节点

--- src/DeleteNodeTwo.java
+++ src/DeleteNodeTwo.java
@@ -1,21 +1,50 @@
+import java.util.ArrayList;
+import java.util.Deque;
+import java.util.LinkedList;
+
 public class DeleteNodeTwo {
 
     public ListNode deleteNode(ListNode head, int val) {
-        if (head == null){
-            return  head;
+//        if (head == null){
+//            return  head;
+//        }
+//        if (head.val == val){
+//            head = head.next;
+//        }
+//        ListNode prev  = head;
+//        ListNode curr = head.next;
+//        while (curr !=null){
+//            /*需要删除的节点*/
+//            if (curr.val == val){
+//                prev.next = curr.next;
+//            }
+//            prev = prev.next;
+//            curr =  curr.next;
+//
+//        }
+//        return  head;
+
+        if (head.val == val){
+            head = head.next;
         }
-        ListNode prev  = head;
-        ListNode curr = head.next;
-        while (curr !=null){
-            /*需要删除的节点*/
-            if (curr.val == val){
-                prev.next = curr.next;
+        Deque<ListNode> deque =  new LinkedList<ListNode>();
+        ListNode curr = head;
+        while (curr!=null){
+            if (curr.val!=val){
+                deque.offer(curr);
             }
-            prev = prev.next;
-            curr =  curr.next;
+            curr = curr.next;
 
         }
-        return  head;
+        ListNode prev = head;
 
+        while (!deque.isEmpty()){
+            prev.next = deque.poll();
+            prev = prev.next;
+        }
+        prev.next = null;
+
+
+        return head;
     }
 }
