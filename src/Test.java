a31f34bce4020e1d26db02802f10c232f5ed31aa
hanxiaosheng <2695621767@qq.com>
2021-11-05 18:35:48 +0800

    647.回文子串.java
    647.回文子串.java

--- src/Test.java
+++ src/Test.java
@@ -132,10 +132,46 @@ public class Test {
         return maxLen;
     }
 
+    public static int countSubstrings(String s) {
+        if(s == null || s.length() == 0){
+            return 0;
+        }
+        int len = s.length();
+        boolean[][]  dp = new boolean[len][len];
+        int  res = 0;
+        for(int l =1;l<=len;l++){
+            for(int i=0;i<=len-l;i++){
+                int j = i+l-1;
+                if(i ==j){
+                    dp[i][j] = true;
+                }
+                char c1 = s.charAt(i);
+                char c2 = s.charAt(j);
+                if (l<=3){
+                    if(c1 == c2){
+                        dp[i][j] = true;
+                    }else{
+                        dp[i][j] = false;
+                    }
+                }else {
+                    if(c1 == c2){
+                        dp[i][j] = dp[i+1][j-1];
+                    }else{
+                        dp[i][j] = false;
+                    }
+                }
+                if(dp[i][j]){
+                    res++;
+                }
+            }
+        }
+        return res;
+    }
+
 
     public static void main(String[] args) {
-        String temp = "aaa";
-        System.out.println(longestPalindromeSubseq(temp));
+        String temp = "abc";
+        System.out.println(countSubstrings(temp));
 
     }
 }
