891bdf40ae6dfbdb3558315785e6d1c4b2a43765
hanxiaosheng <2695621767@qq.com>
2021-11-07 15:40:38 +0800

    md

--- KongFu/113.路径总和-ii.java
+++ KongFu/113.路径总和-ii.java
@@ -23,30 +23,30 @@
 import java.util.*;
 class Solution {
     public List<List<Integer>> pathSum(TreeNode root, int targetSum) {
-           if(root == null){
-               return new ArrayList<List<Integer>>();
-           }
-           Deque<Integer>  deque = new LinkedList<Integer>();
-           List<List<Integer>> res = new ArrayList<List<Integer>>();
-           dfs(root,targetSum,deque,res);
-           return res;
- 
-    }
+        if(root == null){
+            return new ArrayList<List<Integer>>();
+        }
+        Deque<Integer>  deque = new LinkedList<Integer>();
+        List<List<Integer>> res = new ArrayList<List<Integer>>();
+        dfs(root,targetSum,deque,res);
+        return res;
 
-    public void dfs(TreeNode root,int targetSum,Deque<Integer> deque,List<List<Integer>> res){
-           if(root == null){
-               return;
-           }
-           deque.offer(root.val);
-           targetSum -= root.val;
-           if(root.left == null && root.right ==null&& targetSum==0){
-               ArrayList<Integer> temp = new ArrayList<Integer>(deque);
-               res.add(temp);
-           }
-           dfs(root.left,targetSum,deque,res);
-           dfs(root.right,targetSum,deque,res);
-           deque.pollLast();
-    }
+ }
+
+ public void dfs(TreeNode root,int targetSum,Deque<Integer> deque,List<List<Integer>> res){
+        if(root == null){
+            return;
+        }
+        deque.offer(root.val);
+        targetSum -= root.val;
+        if(root.left == null && root.right ==null&& targetSum==0){
+            ArrayList<Integer> temp = new ArrayList<Integer>(deque);
+            res.add(temp);
+        }
+        dfs(root.left,targetSum,deque,res);
+        dfs(root.right,targetSum,deque,res);
+        deque.pollLast();
+ }
 }
 // @lc code=end
 
