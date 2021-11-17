6d1d7329ea4d30f01101112d76eb20ad3445a93e
hanxiaosheng <2695621767@qq.com>
2021-11-17 11:29:00 +0800

    md

--- KongFu/704.二分查找.java
+++ KongFu/704.二分查找.java
@@ -8,18 +8,22 @@
 class Solution {
     public int search(int[] nums, int target) {
         int len = nums.length;
-        int i= 0;
+        if(nums == null || len == 0){
+            return -1;
+        }
+        int i = 0;
         int j = len-1;
         while(i<=j){
-            int mid = i + (j-i)/2;
-            if(target == nums[mid]){
+            int mid = i+(j-i)/2;
+            if(nums[mid] == target){
                 return mid;
-            }else if(target<nums[mid]){
-                j = mid-1;
-            }else if(target>nums[mid]){
+            }else if(nums[mid]<target){
                 i = mid+1;
+            }else if(nums[mid]>target){
+                j= mid-1;
             }
         }
+
         return -1;
 
     }
