354c70afbdd14386a8df648a1c863d7417ccb0ff
hanxiaosheng <2695621767@qq.com>
2021-12-27 09:53:18 +0800

    1

--- src/QuickSort.java
+++ src/QuickSort.java
@@ -1,3 +1,7 @@
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.HashMap;
+import java.util.List;
 
 /*快速排序  分而知之    */
 public class QuickSort {
@@ -36,9 +40,111 @@ public class QuickSort {
 
     }
 
+    public boolean isSameAfterReversals(int num) {
+          if (num == 0){
+              return  true;
+          }
+          String str = String.valueOf(num);
+          int len = str.length();
+          char c = str.charAt(len-1);
+          if (c == '0'){
+              return  false;
+          }
+          return  true;
+    }
+
+
+    /**
+     * 输入：arr = [2,1,3,1,2,3,3]
+     * 输出：[4,2,7,2,4,4,5]
+     * 解释：
+     * - 下标 0 ：另一个 2 在下标 4 ，|0 - 4| = 4
+     * - 下标 1 ：另一个 1 在下标 3 ，|1 - 3| = 2
+     * - 下标 2 ：另两个 3 在下标 5 和 6 ，|2 - 5| + |2 - 6| = 7
+     * - 下标 3 ：另一个 1 在下标 1 ，|3 - 1| = 2
+     * - 下标 4 ：另一个 2 在下标 0 ，|4 - 0| = 4
+     * - 下标 5 ：另两个 3 在下标 2 和 6 ，|5 - 2| + |5 - 6| = 4
+     * - 下标 6 ：另两个 3 在下标 2 和 5 ，|6 - 2| + |6 - 5| = 5
+     * @param arr
+     * @return
+     */
+    public long[] getDistances(int[] arr) {
+              int len = arr.length;
+              if (arr == null || len ==0){
+                  return  new long[0];
+              }
+        HashMap<Integer, List<Integer>>  map = new HashMap<>();
+        for (int i = 0; i < len; i++) {
+            int num = arr[i];
+            if (map.containsKey(num)){
+                List<Integer> index = map.get(num);
+                index.add(i);
+                map.put(num,index);
+            }else {
+                List<Integer> index = new ArrayList<>();
+                index.add(i);
+                map.put(num,index);
+            }
+        }
+        long[] res = new long[len];
+        for (int i = 0; i < len; i++) {
+            int num = arr[i];
+            List<Integer> indexList = map.get(num);
+            if (indexList.size() == 1) {
+                res[i] = 0;
+            }else {
+                int ans = 0;
+                for (int j = 0; j < indexList.size(); j++) {
+                    int index = indexList.get(j);
+                    ans+= Math.abs(i-index);
+                }
+                res[i] = ans;
+            }
+        }
+        return  res;
+    }
+
+    public long[] getDistances1(int[] arr) {
+        HashMap<Integer, ArrayList<Integer>> map = new HashMap<>();
+        for (int i = 0; i < arr.length; i++) {
+            map.computeIfAbsent(arr[i], t -> new ArrayList<>()).add(i);
+        }
+        long[] result = new long[arr.length];
+        for (ArrayList<Integer> list : map.values()) {
+            for (int i : list) {
+                result[list.get(0)] += i - list.get(0);
+            }
+            for (int i = 1; i < list.size(); i++) {
+                result[list.get(i)] = result[list.get(i - 1)] + (2 * i - list.size()) * (list.get(i) - list.get(i - 1));
+            }
+        }
+        return result;
+    }
+
+
+    public void  dfsOfExecute(int n,int row,int col,char ch){
+
+    }
+
+
+    public int[] recoverArray(int[] nums) {
+          int len = nums.length;
+          int n = len/2;
+        Arrays.sort(nums);
+        int[] res = new int[n];
+        for (int i = 0; i <n ; i++) {
+            int right = i+n;
+            int mid = (nums[i] + nums[right])/2;
+            res[i] = mid;
+        }
+        return  res;
+    }
+
+
     public static void main(String[] args) {
 
 //        removeKdigits("1593212",3);
+//         知名应用  知名公司  镀金
         int[] arr = new int[] {9,4,6,8,3,10,6,6,6,6,7,22,11,2333,3333,4,53,90};
         quickSort(arr,0,arr.length-1);
         for (int i = 0; i < arr.length; i++) {
@@ -46,6 +152,8 @@ public class QuickSort {
             System.out.print(arr[i]);
         }
     }
+
+    // 乐视超级电视  出现新的情况
 
 
 }
--- src/Test.java
+++ src/Test.java
@@ -6,36 +6,31 @@ import java.util.*;
 // 人生 那种挑战的欲望
 
 public class Test {
-    HashMap<Character,char[]> map = new HashMap<>();
-    public List<String> letterCombinations(String digits) {
-            map.put('2',new char[]{'a','b','c'});
-        map.put('3',new char[]{'d','e','f'});
-        map.put('4',new char[]{'g','h','i'});
-        map.put('5',new char[]{'j','k','l'});
-        map.put('6',new char[]{'m','n','o'});
-        map.put('7',new char[]{'p','q','r','s'});
-        map.put('8',new char[]{'t','u','v'});
-        map.put('9',new char[]{'w','x','y','z'});
-        char[]  digitsArr = digits.toCharArray();
-        int len = digitsArr.length;
-        List<char[]> list = new ArrayList<>();
-        for (int i = 0; i < len; i++) {
-             char ch = digitsArr[i];
-             char[] values = map.get(ch);
-               list.add(values);
-        }
-        int size = list.size();
-        StringBuilder sb = new StringBuilder();
-        for (char[] values:list){
-            int l1 = values.length;
-            for (int i = 0; i < l1; i++) {
-//                sb.append(values[i])
-            }
-
-        }
+    public int[] MySort (int[] arr) {
+        // write code here
+        quick(arr,0,arr.length-1);
+        return  arr;
 
     }
 
+    public void  quick(int[] nums,int i,int j){
+        if (i>j){
+            return;
+        }
+        int left = i;
+        int right = j ;
+        int pviot = nums[left];
+        while (left<right){
+            while (left<right&&nums[right]>pviot) right--;
+            nums[left] = nums[right];
+            while (left<right&&nums[left]<=pviot)left++;
+            nums[right] = nums[left];
+        }
+        int mid = left;
+        nums[mid] = pviot;
+        quick(nums,i,mid-1);
+        quick(nums,mid+1,j);
+    }
 
 
 
