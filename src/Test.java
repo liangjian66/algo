ff516c0068c8a3ece5b3e214d7830ffd49fe2775
韩小胜 <2695621767@qq.com>
2022-01-19 15:50:42 +0800

    存在重复元素

--- src/Test.java
+++ src/Test.java
@@ -7,6 +7,37 @@ import java.util.*;
 
 public class Test {
 
+    public boolean containsNearbyDuplicate(int[] nums, int k) {
+      int len = nums.length;
+      if (nums == null || len == 0){
+          return true;
+      }
+      HashMap<Integer,List<Integer>> map = new HashMap<>();
+        for (int i = 0; i < len; i++) {
+            int num = nums[i];
+            if (map.containsKey(num)){
+                List<Integer> list = map.get(num);
+                list.add(i);
+            }else {
+                List<Integer> list = new ArrayList<>();
+                list.add(i);
+                map.put(num,list);
+            }
+        }
+        for (List<Integer> list: map.values()){
+              int size = list.size();
+            for (int i = 0; i+1 < size; i++) {
+                if (Math.abs(list.get(i+1)-list.get(i))<=k){
+                    return  true;
+                }
+            }
+        }
+
+        return  false;
+    }
+
+
+
 
     public int trap(int[] height) {
          int len = height.length;
