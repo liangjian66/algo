7cb62d886984a0523dbef9b79cec06a316fed76e
韩小胜 <2695621767@qq.com>
2022-01-24 14:39:01 +0800

    前序遍历之迭代解法
    前序遍历之迭代解法

--- src/Test.java
+++ src/Test.java
@@ -19,7 +19,22 @@ public class Test {
     }
 
 
-    public int trap(int[] height) {
+    public List<Integer> preorderTraversal(TreeNode root) {
+        List<Integer> res = new ArrayList<>();
+        if (root == null )return  res;
+        Stack<TreeNode> stack = new Stack<>();
+        stack.push(root);
+        while (!stack.isEmpty()){
+            TreeNode node = stack.pop();
+            res.add(node.val);
+            if (node.right != null) stack.push(node.right);
+            if (node.left != null) stack.push(node.left);
+        }
+        return  res;
+    }
+
+
+    public int trap1(int[] height) {
           int len = height.length;
           if (len == 0|| height == null){
               return 0;
