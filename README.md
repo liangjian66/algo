b5c7970048dc9353f3c3f61941d469ed2726b211
韩小胜 <2695621767@qq.com>
2022-01-25 14:25:54 +0800

    二叉搜索树中的插入操作
    二叉搜索树中的插入操作

--- README.md
+++ README.md
@@ -812,7 +812,7 @@ result = []
 * [二叉搜索树中的众数](https://leetcode-cn.com/problems/find-mode-in-binary-search-tree/solution/er-cha-sou-suo-shu-zhong-de-zhong-shu-by-coz9/)
 * [二叉树的最近公共祖先](https://leetcode-cn.com/problems/lowest-common-ancestor-of-a-binary-tree/solution/er-cha-shu-de-zui-jin-gong-gong-zu-xian-koc45/)
 * [二叉搜索树的最近公共祖先](https://leetcode-cn.com/problems/lowest-common-ancestor-of-a-binary-search-tree/solution/er-cha-sou-suo-shu-de-zui-jin-gong-gong-pdrot/)
-
+* [二叉搜索树中的插入操作](https://leetcode-cn.com/problems/insert-into-a-binary-search-tree/solution/er-cha-sou-suo-shu-zhong-de-cha-ru-cao-z-031h/)
 #  算法图书
 > 学习最笨的办法就是看书拉，也是最聪明的办法 ， 博览群书总会有不一样的收获。
 * 数据结构与算法分析：C语言描述_原书第2版_高清版
--- src/Test.java
+++ src/Test.java
@@ -19,7 +19,30 @@ public class Test {
     }
 
 
-
+    public TreeNode insertIntoBST(TreeNode root, int val) {
+        if (root == null){
+            return  new TreeNode(val);
+        }
+        TreeNode ptr = root;
+        while (ptr != null){
+            if (val<ptr.val){
+                 if (ptr.left == null){
+                    ptr.left = new TreeNode(val);
+                    break;
+                 }else {
+                     ptr = ptr.left;
+                 }
+            }else {
+                   if (ptr.right == null){
+                        ptr.right = new TreeNode(val);
+                        break;
+                   }else {
+                         ptr = ptr.right;
+                   }
+            }
+        }
+        return  root;
+    }
 
 
 
