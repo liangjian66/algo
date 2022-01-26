a6bebc3770fa9511dda8c5a7fcc0799ba73099d7
韩小胜 <2695621767@qq.com>
2022-01-26 18:02:02 +0800

    N 皇后
    N 皇后

--- src/Test.java
+++ src/Test.java
@@ -14,6 +14,58 @@ public class Test {
         Test test = new Test();
       System.out.println(test.isAnagram("rat","car"));
     }
+    public  List<List<String>> solveNQueens(int n){
+        // 分别用三个set集合记录不同列，两条对角线的占用情况
+        Set<Integer> columnSet = new HashSet<>();
+        Set<Integer>   diagonalSet1 = new HashSet<>();
+        Set<Integer>  diagonalSet2 = new HashSet<>();
+        // 初始化结果集合
+        List<List<String>> result = new ArrayList<>();
+        String[][] current = new String[n][n];
+        for (String[] s:current){
+             Arrays.fill(s,".");
+        }
+        backTrack(result, 0, n, current, columnSet, diagonalSet1, diagonalSet2);
+        return result;
+
+    }
+
+    public void  backTrack(List<List<String>> result,int row,int n ,String[][] current,Set<Integer>  columnSet,Set<Integer> diagonalSet1,Set<Integer> diagonalSet2){
+          if (row == n){
+               result.add(transfer(current));
+          }else {
+              for (int column = 0; column <n ; column++) {
+                  if (columnSet.contains(column) || diagonalSet1.contains(column - row)|| diagonalSet2.contains(column+row)){
+                      continue;
+                  }
+                  // 该位置符合要求，放置棋子，并在序号集合中 占用 行 ，主对角线  次对角线的序号
+                  current[row][column] = "Q";
+                  columnSet.add(column);
+                  diagonalSet1.add(column-row);
+                  diagonalSet2.add(column+row);
+                  // 向下一行递归
+                  backTrack(result, row + 1, n, current, columnSet, diagonalSet1, diagonalSet2);
+                  columnSet.remove(column);
+                  diagonalSet1.remove(column-row);
+                  diagonalSet2.remove(column+row);
+
+              }
+          }
+    }
+
+    public List<String> transfer(String[][] current) {
+        String[] result = new String[current.length];
+        for (int i = 0; i < current.length; ++i) {
+            StringBuilder stringBuilder = new StringBuilder();
+            for (int j = 0; j < current[i].length; ++j) {
+                stringBuilder.append(current[i][j]);
+            }
+            result[i] = stringBuilder.toString();
+        }
+        return Arrays.asList(result);
+    }
+
+
 
 
     public List<List<Integer>> permuteUnique(int[] nums) {
