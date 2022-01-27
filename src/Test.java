78e1427e95c644e1defbf482e75a449b9ccf5d25
韩小胜 <2695621767@qq.com>
2022-01-27 15:54:16 +0800

    不同路径
    不同路径

--- src/Test.java
+++ src/Test.java
@@ -15,6 +15,23 @@ public class Test {
       System.out.println(test.isAnagram("rat","car"));
     }
 
+
+    public int uniquePaths(int m, int n) {
+        int[][] dp = new int[m+1][n+1];
+        for (int i = 1; i <=m ; i++) {
+            dp[i][1]  = 1;
+        }
+        for (int i = 1; i <n ; i++) {
+            dp[1][i] = 1;
+        }
+        for (int i = 2; i < m+1; i++) {
+            for (int j = 2; j <n+1 ; j++) {
+             dp[i][j] = dp[i-1][j]+ dp[i][j-1];
+            }
+        }
+        return  dp[m][n];
+    }
+
     public int minCostClimbingStairs(int[] cost) {
          int len = cost.length;
          if (cost == null || len == 0){
